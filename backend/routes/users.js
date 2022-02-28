3308699cce3453cd9fd354869dff00b5c1d106d6
Glebkas <baran123456@gmail.com>
2022-02-28 17:27:00 +0200

    Update users.js

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,29 +1,31 @@
 const router = require('express').Router();
-const { celebrate, Joi } = require('celebrate');
+const { celebrate, Joi, Segments } = require('celebrate');
 
 const {
   getAllUsers, getUserById, getUserMe, updateUserAvatar, updateUserInfo,
 } = require('../controllers/users');
 
-router.get('/', getAllUsers);
 router.get('/users/me', getUserMe);
-router.get('/users/:id', celebrate({
-  params: Joi.object().keys({
-    id: Joi.string().hex().length(24),
-  }),
-}), getUserById);
 
 router.patch('/users/me', celebrate({
-  body: Joi.object().keys({
+  [Segments.BODY]: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
     about: Joi.string().required().min(2).max(30),
   }),
 }), updateUserInfo);
 
 router.patch('/users/me/avatar', celebrate({
-  body: Joi.object().keys({
-    avatar: Joi.string().required().pattern(/^https?:\/{2}(www\.)?[a-z\0-9]{1,}\.[a-z]{1,}(\/[a-z0-9._~:/?%#@!$&'[\]()*+,;=]*)?/),
+  [Segments.BODY]: Joi.object().keys({
+    avatar: Joi.string().required().pattern(/^(http|https):\/\/[^ "]+$/),
   }),
 }), updateUserAvatar);
+
+router.get('users/:id', celebrate({
+  [Segments.PARAMS]: Joi.object().keys({
+    id: Joi.string().hex().required().length(24),
+  }),
+}), getUserById);
+
+router.get('/users', getAllUsers);
 
 module.exports = router;
