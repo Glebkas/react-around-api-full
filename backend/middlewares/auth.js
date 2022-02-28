94bc23b9e6247b859b35e2b4154bb15ac9f4a800
Glebkas <baran123456@gmail.com>
2022-02-28 08:46:56 +0200

    Create auth.js

---
+++ backend/middlewares/auth.js
@@ -0,0 +1,22 @@
+const { NODE_ENV, JWT_SECRET } = process.env;
+const jwt = require('jsonwebtoken');
+const AuthorizationError = require('../errors/authorization-error');
+
+// eslint-disable-next-line consistent-return
+const auth = (req, res, next) => {
+  const { authorization } = req.headers;
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    throw new AuthorizationError('Unauthorized client');
+  }
+  const token = authorization.replace('Bearer ', '');
+  let payload;
+  try {
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
+  } catch (err) {
+    throw new AuthorizationError('Unauthorized client');
+  }
+  req.user = payload;
+  next();
+};
+
+module.exports = auth;
